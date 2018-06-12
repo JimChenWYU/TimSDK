a3477b333bb6e68ef1e4959de9d9a9f3345ee352
JimChen <18219111672@163.com>
2018-06-13 00:09:26 +0800

    添加日志库

---
+++ src/Foundation/Application.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lenovo
+ * Date: 6/12/2018
+ * Time: 11:58 PM
+ */
+
+class Application
+{
+
+}
---
+++ src/Foundation/Config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lenovo
+ * Date: 6/13/2018
+ * Time: 12:01 AM
+ */
+
+namespace TimSDK\Foundation\Application;
+
+
+class Config
+{
+
+}
