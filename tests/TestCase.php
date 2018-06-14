8695fb1c466e7007c67e1dc3b26261cc2e536ee9
JimChen <18219111672@163.com>
2018-06-14 13:45:38 +0800

    初始化单元测试环境

---
+++ tests/TestCase.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lenovo
+ * Date: 6/13/2018
+ * Time: 8:17 PM
+ */
+namespace TimSDK\Tests;
+
+use PHPUnit\Framework\TestCase as BaseTestCase;
+
+class TestCase extends BaseTestCase
+{
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+
+define('TEST_ROOT', __DIR__);
+
+require __DIR__ . '/../vendor/autoload.php';
