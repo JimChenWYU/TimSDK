5ebcee5a810b2dc6e46aac5ad3c37cae3c88dc56
JimChen <18219111672@163.com>
2018-06-15 10:38:18 +0800

    异常添加

---
+++ src/Core/Exceptions/MissingArgumentsException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lenovo
+ * Date: 6/14/2018
+ * Time: 8:25 PM
+ */
+
+namespace TimSDK\Core\Exceptions;
+
+use TimSDK\Core\Exception;
+
+class MissingArgumentsException extends Exception
+{
+}
---
+++ src/Core/Exceptions/NotInstantiableException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lenovo
+ * Date: 6/15/2018
+ * Time: 10:07 AM
+ */
+
+namespace TimSDK\Core\Exceptions;
+
+use TimSDK\Core\Exception;
+
+class NotInstantiableException extends Exception
+{
+}
