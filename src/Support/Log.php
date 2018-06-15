41728809f0801f26484e21437324e84a16f0a35c
JimChen <18219111672@163.com>
2018-06-15 10:40:10 +0800

    添加日志辅助类

--- src/Support/Log.php
+++ src/Support/Log.php
@@ -5,6 +5,7 @@ use Monolog\Handler\ErrorLogHandler;
 use Monolog\Handler\NullHandler;
 use Monolog\Logger as Monolog;
 use Psr\Log\LoggerInterface;
+use TimSDK\Core\Exceptions\NotInstantiableException;
 
 /**
  * Class Log
@@ -21,68 +22,9 @@ use Psr\Log\LoggerInterface;
  */
 class Log
 {
-    protected static $logger;
-
-    /**
-     * Return the logger instance.
-     *
-     * @return \Psr\Log\LoggerInterface
-     */
-    public static function getLogger()
+    private function __construct()
     {
-        return self::$logger ?: self::$logger = self::createDefaultLogger();
-    }
-
-    /**
-     * Set logger.
-     *
-     * @param \Psr\Log\LoggerInterface $logger
-     */
-    public static function setLogger(LoggerInterface $logger)
-    {
-        self::$logger = $logger;
-
-        return self::$logger;
-    }
-
-    /**
-     * Tests if logger exists.
-     *
-     * @return bool
-     */
-    public static function hasLogger()
-    {
-        return self::$logger ? true : false;
-    }
-
-    /**
-     * Make a default log instance.
-     *
-     * @return \Monolog\Logger
-     */
-    private static function createDefaultLogger()
-    {
-        $log = new Monolog('TimSDK');
-
-        if (defined('PHPUNIT_RUNNING') || 'cli' === php_sapi_name()) {
-            $log->pushHandler(new NullHandler());
-        } else {
-            $log->pushHandler(new ErrorLogHandler());
-        }
-
-        return $log;
-    }
-
-    /**
-     * Forward call.
-     *
-     * @param string $method
-     * @param array  $arguments
-     * @return mixed
-     */
-    public function __call($method, $arguments)
-    {
-        return call_user_func_array([self::getLogger(), $method], $arguments);
+        throw new NotInstantiableException(__CLASS__ . ' can not instantiate.');
     }
 
     /**
@@ -95,6 +37,6 @@ class Log
      */
     public static function __callStatic($method, $arguments)
     {
-        return forward_static_call_array([self::getLogger(), $method], $arguments);
+        return forward_static_call_array([timsdk_app('log'), $method], $arguments);
     }
 }
