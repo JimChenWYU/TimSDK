f2b7e323e093733e6bded27ad8a13dd52fb41b99
JimChen <18219111672@163.com>
2018-06-15 16:58:33 +0800

    更新PHPunit

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,3 +3,8 @@
 define('TEST_ROOT', __DIR__);
 
 require __DIR__ . '/../vendor/autoload.php';
+
+function phpunit_env($name, $default = null)
+{
+    return \TimSDK\Support\Arr::get(get_defined_constants(true), 'user.' . strtoupper($name), $default);
+}
