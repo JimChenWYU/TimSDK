1ed7e45813a6355895fa6843000971502fdc7bf9
JimChen <18219111672@163.com>
2018-06-19 20:18:21 +0800

    简化容器

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 
 define('TEST_ROOT', __DIR__);
+define('PROJECT_ROOT', rtrim(dirname(__DIR__), DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR . 'src');
 
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -9,7 +10,7 @@ function phpunit_env($name, $default = null)
     return \TimSDK\Support\Arr::get(get_defined_constants(true), 'user.' . strtoupper($name), $default);
 }
 
-if (! function_exists('dd')) {
+if (!function_exists('dd')) {
     function dd()
     {
         call_user_func_array('timsdk_dd', func_get_args());
