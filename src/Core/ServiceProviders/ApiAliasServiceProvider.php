b11dad31818ec8a21ea5922fba9b536bed1dd778
JimChen <18219111672@163.com>
2018-06-24 12:33:46 +0800

    style(ApiAliasServiceProvider): remove unnecessary arguments.

--- src/Core/ServiceProviders/ApiAliasServiceProvider.php
+++ src/Core/ServiceProviders/ApiAliasServiceProvider.php
@@ -24,7 +24,7 @@ class ApiAliasServiceProvider extends ServiceProvider
      */
     public function register(Container $pimple)
     {
-        $pimple['apiAlias'] = function ($pimple) {
+        $pimple['apiAlias'] = function () {
             return new ApiAlias();
         };
     }
