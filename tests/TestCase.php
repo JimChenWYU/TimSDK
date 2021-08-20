563f2858abf7e4cda2079ab04f868ed62a73f54c
JimChen <imjimchen@163.com>
2021-08-20 10:16:13 +0800

    wip

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,7 +8,52 @@
 namespace TimSDK\Tests;
 
 use PHPUnit\Framework\TestCase as BaseTestCase;
+use TimSDK\Kernel\ServiceContainer;
 
 class TestCase extends BaseTestCase
 {
+	/**
+	 * Create API Client mock object.
+	 *
+	 * @param string                                   $name
+	 * @param array|string                             $methods
+	 * @param \TimSDK\Kernel\ServiceContainer|null $app
+	 *
+	 * @return \Mockery\Mock
+	 */
+	public function mockApiClient($name, $methods = [], ServiceContainer $app = null)
+	{
+		$methods = implode(',', array_merge([
+			'httpGet', 'httpPost', 'httpPostJson', 'request', 'registerMiddlewares', 'castRequestQuery',
+		], (array) $methods));
+
+		$client = \Mockery::mock(
+			$name."[{$methods}]",
+			[ $app ?? \Mockery::mock(ServiceContainer::class), ]
+		)->shouldAllowMockingProtectedMethods();
+		$client->allows()->registerHttpMiddlewares()->andReturnNull();
+
+		return $client;
+	}
+
+	/**
+	 * Tear down the test case.
+	 */
+	public function tearDown(): void
+	{
+		$this->finish();
+		parent::tearDown();
+		if ($container = \Mockery::getContainer()) {
+			$this->addToAssertionCount($container->Mockery_getExpectationCount());
+		}
+		\Mockery::close();
+	}
+
+	/**
+	 * Run extra tear down code.
+	 */
+	protected function finish()
+	{
+		// call more tear down methods
+	}
 }
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,10 +4,3 @@ define('TEST_ROOT', __DIR__);
 define('PROJECT_ROOT', rtrim(dirname(__DIR__), DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR . 'src');
 
 require __DIR__ . '/../vendor/autoload.php';
-
-function phpunit_env($name, $default = null)
-{
-    $value = \TimSDK\Support\Arr::get(get_defined_constants(true), 'user.' . strtoupper($name), $default);
-
-    return $value !== '0' && empty($value) ? $default : $value;
-}
