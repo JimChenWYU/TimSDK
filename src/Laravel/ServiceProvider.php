c663eef2dd7e5e35feff210c4a8b2de56fb4e7ae
JimChen <imjimchen@163.com>
2021-08-25 14:34:08 +0800

    wip

--- src/Laravel/ServiceProvider.php
+++ src/Laravel/ServiceProvider.php
@@ -19,7 +19,12 @@ class ServiceProvider extends LaravelServiceProvider
         $this->mergeConfigFrom(__DIR__ . '/config/tim-sdk.php', 'tim-sdk');
 
         $this->app->singleton(TimSDK::class, function () {
-            return new TimSDK(config('tim-sdk'));
+            return tap(new TimSDK(config('tim-sdk')), function (TimSDK $tim) {
+            	// 默认使用 Laravel 缓存系统 （已实现 PSR16 规范）
+	            $tim->rebind('cache', $this->app['cache.store']);
+	            // 默认使用 Laravel Request (使用symfony/http-foundation)
+	            $tim->rebind('request', $this->app['request']);
+            });
         });
 
         $this->app->alias(TimSDK::class, 'tim-sdk');
