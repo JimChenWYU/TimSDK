5ba0047b59f8a408f7f7e4dd21b839c51af61e4b
JimChenWYU <JimChenWYU@users.noreply.github.com>
2021-08-26 02:15:23 +0000

    Fix styling

--- src/Laravel/ServiceProvider.php
+++ src/Laravel/ServiceProvider.php
@@ -19,16 +19,17 @@ class ServiceProvider extends LaravelServiceProvider
         $this->mergeConfigFrom(__DIR__ . '/config/tim-sdk.php', 'tim-sdk');
 
         $this->app->singleton(TimSDK::class, function () {
-        	/** @var array $config */
-	        $config = array_merge(
-	        	config('tim-sdk.defaults', []),
-		        config('tim-sdk.options.connections.'.config('tim-sdk.options.default'), []));
+            /** @var array $config */
+            $config = array_merge(
+                config('tim-sdk.defaults', []),
+                config('tim-sdk.options.connections.'.config('tim-sdk.options.default'), [])
+            );
             return tap(new TimSDK($config), function (TimSDK $tim) {
-            	if (config('tim-sdk.defaults.use_laravel_cache')) {
-		            $tim->rebind('cache', $this->app['cache.store']);
-	            }
-	            // 默认使用 Laravel Request (使用symfony/http-foundation)
-	            $tim->rebind('request', $this->app['request']);
+                if (config('tim-sdk.defaults.use_laravel_cache')) {
+                    $tim->rebind('cache', $this->app['cache.store']);
+                }
+                // 默认使用 Laravel Request (使用symfony/http-foundation)
+                $tim->rebind('request', $this->app['request']);
             });
         });
 
--- src/Laravel/config/tim-sdk.php
+++ src/Laravel/config/tim-sdk.php
@@ -2,57 +2,57 @@
 
 return [
 
-	'defaults' => [
-		/*
+    'defaults' => [
+        /*
          * 使用 Laravel 的缓存系统
          */
-		'use_laravel_cache' => true,
+        'use_laravel_cache' => true,
 
-		/**
-		 * http 客户端配置
-		 */
-		'http'       => [
-			'timeout' => 30.0,
-			'max_retries' => 2,
-			'retry_delay' => 500,
-		],
+        /**
+         * http 客户端配置
+         */
+        'http' => [
+            'timeout' => 30.0,
+            'max_retries' => 2,
+            'retry_delay' => 500,
+        ],
 
-		/**
-		 * 日志配置
-		 */
-		'log' => [
-			'default' => 'single',
-			'channels' => [
-				'single' => [
-					'driver' => 'single',
-					'path'  => storage_path('logs/tim-sdk.log'),
-					'level' => 'debug',
-				],
-			],
-		],
+        /**
+         * 日志配置
+         */
+        'log' => [
+            'default' => 'single',
+            'channels' => [
+                'single' => [
+                    'driver' => 'single',
+                    'path' => storage_path('logs/tim-sdk.log'),
+                    'level' => 'debug',
+                ],
+            ],
+        ],
 
-		/**
-		 * 事件监听
-		 */
-		'events' => [
-			'listen' => [
-				//
-			],
-		],
-	],
+        /**
+         * 事件监听
+         */
+        'events' => [
+            'listen' => [
+                //
+            ],
+        ],
+    ],
 
     'options' => [
-    	'default' => 'main',
+        'default' => 'main',
 
-	    'connections' => [
-		    'main' => [
-			    // 应用id
-			    'app_id' => env('TIM_APP_ID'),
-			    // 应用秘钥
-			    'key' => env('TIM_KEY'),
-			    // 默认管理员账号
-			    'identifier' => 'administrator',
-		    ],
-	    ],
+        'connections' => [
+            'main' => [
+                // 应用id
+                'app_id' => env('TIM_APP_ID'),
+                // 应用秘钥
+                'key' => env('TIM_KEY'),
+                // 默认管理员账号
+                'identifier' => 'administrator',
+            ],
+        ],
     ],
 ];
